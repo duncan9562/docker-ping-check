a755a5161a94eb387e35b247dbe8e32862241ab8
Duncan Mountford <duncan9562@gmail.com>
2018-08-16 16:25:05 +0100

    Simplified the output, returning OK or FAILED depending on the response code from ping

--- ping.sh
+++ ping.sh
@@ -13,9 +13,16 @@ fi
 ip=$1
 
 # Ping address
-ping -c 4 $1
+ping -q -c 4 $1 >/dev/null 2>&1
 
+#Capture return code
+return_code=$?
 
-
-
-
+#Test return code, and print output accordingly
+if [ "$return_code" -eq "0" ]; then
+    echo "OK"
+    exit
+else
+    echo "CHECK FAILED!"
+    exit
+fi
